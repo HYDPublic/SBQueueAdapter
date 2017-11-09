86b186dfcdfa208054e3d6500a24f7ccca4bff83
ferra71@hotmail.com <https://ibizsolutions.visualstudio.com/DefaultCollection/APL/_git/Shared.Schemas.EHM>
2017-11-09 21:00:40 +0100

    init service bus adapter

---
+++ Admin/SBQueueAdapterManagement.cs
@@ -0,0 +1,150 @@
+
+using System;
+using System.Data.OleDb;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using Microsoft.BizTalk.Adapter.Framework;
+using Microsoft.BizTalk.Component.Interop;
+using System.Reflection;
+
+namespace BizTalk.Adapter.SBQueueAdmin
+{
+    /// <summary>
+    /// Summary description for AdapterManagement.
+    /// </summary>
+    [CLSCompliant(false)]
+    public class SBQueueAdapterManagement :
+        IAdapterConfig,
+        IStaticAdapterConfig,
+        IAdapterConfigValidation
+    {
+        public SBQueueAdapterManagement()
+        {
+        }
+
+        #region IStaticAdapterConfig Members
+
+        public string GetServiceOrganization(IPropertyBag endPointConfiguration, string nodeIdentifier)
+        {
+            return null;
+        }
+
+
+
+        #endregion
+        //  IAdapterConfig
+        public string GetConfigSchema(ConfigType type)
+        {
+            switch (type)
+            {
+
+                case ConfigType.ReceiveHandler:
+                    return GetSchemaFromResource("BizTalk.Adapter.SBQueueAdmin.ReceiveHandler.xsd");
+
+                case ConfigType.ReceiveLocation:
+                    return GetSchemaFromResource("BizTalk.Adapter.SBQueueAdmin.ReceiveLocation.xsd");
+            }
+
+            return String.Empty;
+        }
+
+        private string GetSchemaFromResource(string name)
+        {
+            Assembly assem = this.GetType().Assembly;
+            using (Stream stream = assem.GetManifestResourceStream(name))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string schema = reader.ReadToEnd();
+                    return schema;
+                }
+            }
+        }
+        // used to get the WSDL file name for the selected WSDL.
+        public string[] GetServiceDescription(string[] wsdls)
+        {
+            string[] result = null;
+            return result;
+        }
+
+        //used to acquire externally referenced xsd's
+        public Result GetSchema(string xsdLocation, string xsdNamespace, out string xsdFileName)
+        {
+            xsdFileName = string.Empty;
+            return Result.Continue;
+        }
+
+        public string ValidateConfiguration(ConfigType type, string config)
+        {
+            try
+            {
+                switch (type)
+                {
+
+
+                    case ConfigType.ReceiveHandler:
+                        return config;
+
+                    case ConfigType.ReceiveLocation:
+                        return ValidateLocation(config);
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// Generate uri entry based on directory and fileMask values
+        /// </summary>
+        /// <param name="config">Instance value to be validated</param>
+        /// <returns>Validated configuration.</returns>
+        private string ValidateLocation(string config)
+        {
+
+            // Load up document
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(config);
+
+            XmlNode nodeQueue = document.SelectSingleNode("Config/queue");
+            if (null == nodeQueue)
+            {
+                nodeQueue = document.CreateElement("queue");
+                document.DocumentElement.AppendChild(nodeQueue);
+            }
+
+            XmlNode nodeEndpoint = document.SelectSingleNode("Config/endpoint");
+            if (null == nodeEndpoint)
+            {
+                nodeEndpoint = document.CreateElement("queue");
+                document.DocumentElement.AppendChild(nodeEndpoint);
+            }
+
+            var slash = nodeEndpoint.InnerText.EndsWith("/") ? String.Empty : "/";
+
+
+            XmlNode uri = document.SelectSingleNode("Config/uri");
+            if (null == uri)
+            {
+                uri = document.CreateElement("uri");
+                document.DocumentElement.AppendChild(uri);
+            }
+
+            uri.InnerText = String.Format("{0}{1}{2}", nodeEndpoint.InnerText, slash, nodeQueue.InnerText);
+
+
+
+            return document.OuterXml;
+        }
+
+    }
+}
---
+++ Deployment/Install.ps1
@@ -0,0 +1,38 @@
+﻿
+
+$adminDLL = "..\Admin\bin\Release\BizTalk.Adapter.SBQueueAdmin.dll";
+$receiveDLL = "..\Runtime\bin\Release\BizTalk.Adapter.SBQueue.dll";
+$baseAdapterDLL = "..\BaseAdapter\v1.0.2\bin\Release\Microsoft.BizTalk.Adapter.Common.dll";
+
+#Test for 64 bit
+$Is64Bit =  [Environment]::Is64BitProcess;
+
+#Install Path
+$InstallPath = "C:\Program Files\SBQueueAdapter\";
+If($Is64Bit) 
+{
+    Write-Host "64-bit Process detected."
+    $InstallPath = "C:\Program Files (x86)\SBQueueAdapter\"; 
+}
+
+#Copy RELEASE version
+If((Test-Path $adminDLL) -and (Test-Path $receiveDLL) -and (Test-Path $baseAdapterDLL)) 
+{
+    Write-Host "Release version of adapter DLL found"
+    xcopy /Y $adminDLL $InstallPath
+    xcopy /Y $receiveDLL $InstallPath
+    xcopy /Y $baseAdapterDLL $InstallPath
+
+    #Registry entries
+    Write-Host "Merging registry entries";
+    regedit /S .\SBQueueAdapter.reg
+
+    if($Is64Bit) {
+        Write-Host "Merging 32-bit on 64-bit registry entries";
+        regedit /S .\SBQueueAdapter64.reg
+    }
+
+
+} else {
+    Write-Error "Release version of adapter DLL NOT found. Please build the VS project first in RELEASE mode."
+}
